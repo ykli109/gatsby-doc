0d6f12d9b3a671eb63f43b0022124df9e4a70247
liyunkun <ykli109@outlook.com>
2021-05-08 17:50:52 +0800

    [feat] 文档首页目录支持自动生成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,40 @@
 /*
  * @file: file
- * @author: 
+ * @author: 首页
  */
 import * as React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
+import _ from 'lodash';
 
-const IndexPage = () => {
+export const pageQuery = graphql`
+  {
+    allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              pathname,
+              title
+            }
+          }
+        }
+    }
+  }
+`
+
+type LinkParams = {path: string; title: string;}
+
+const IndexPage = ({ data }: { data: ObjectType }) => {
+  const {edges = []} = data.allMarkdownRemark;
+
+  const links: Array<LinkParams> = edges.map(({ node }: any) => ({
+    path: node.fields.pathname,
+    title: node.fields.title
+  }));
+
   return (
     <main>
-      <h1>Index Page</h1>
-      <p><Link to="/React18/Automatic Batching">Automatic Batching</Link></p>
-      <p><Link to="/HTTP演进">HTTP演进</Link></p>
-      <p><Link to="/Vue中的核心概念">Vue核心概念</Link></p>
+      <h1>参考文档</h1>
+      {links.map(link => <p key={link.path}><Link to={link.path}>{link.title}</Link></p>)}
     </main>
   )
 }
